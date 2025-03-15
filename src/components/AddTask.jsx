8115b48c975a41aa5a38214d8c8aa524f95fc55e
Vikas Solanki <solankivikas066@gmail.com>
2025-03-15 18:47:16 +0530

    Add todo header

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div className='add-task-container'>
+      +
+    </div>
+  )
+}
+
+export default AddTask
---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import AddTask from './AddTask'
+import './Todo.css'
+
+const Todo = () => {
+  return (
+    <div className='todo-container'>
+      <h1 className='todo-title'>todo list</h1>
+      <div className='todo-header'>
+        <input type="text" className='search' placeholder='Search note...'/>
+        <button className="sort">All</button>
+        <button>Theme</button>
+      </div>
+      <AddTask />
+    </div>
+  )
+}
+
+export default Todo
